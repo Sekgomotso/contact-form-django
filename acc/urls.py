d9231a7aabd548b258badd1a31ac9d5fd51060af
Sekgomotso_s <motsoshalang@gmail.com>
2020-05-12 10:02:20 +0200

    a start

--- acc/urls.py
+++ acc/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     # form route
     path('', views.home_page, name='home_page'),
 
-    # # registered route
-    # path('registered/', views.registered, name='form-registered'),
 ]
--- acc/views.py
+++ acc/views.py
@@ -14,20 +14,3 @@ def home_page(request):
          ['wifow52238@inbov03.com'], 
          fail_silently=False )
     return render(request, 'acc/form.html')
-
-# # thank you page
-# def registered(request):
-#     return render(request, 'acc/registered.html')
-
-# from django.core import mail
-# from django.template.loader import render_to_string
-# from django.utils.html import strip_tags
-# from django.conf import settings
-
-# subject = 'Registration form'
-# html_message = render_to_string('mail_form.html', {'context': 'values'})
-# plain_message = strip_tags(html_message)
-# from_email = settings.EMAIL_HOST_USER
-# to = ['wifow52238@inbov03.com']
-
-# mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
